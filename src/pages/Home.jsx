7eef39309e6679c2eb944bdad25e2f92c334b5c1
David Mendoza <dmendozasommer@gmail.com>
2024-05-21 13:59:19 +0200

    Started with the grain filter for main photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,24 @@ export default function HomePage() {
   return (
     <>
       <div id="home">
-        <motion.div
+        <motion.div id="background_blue"></motion.div>
+        {/* <motion.div
           initial={{ scale: 0.5, y: 500 }}
           animate={{ scale: 1.2, y: 0, transition: { duration: 0.7 } }}
           exit={{ scale: 0.5, y: 500, transition: { duration: 0.4 } }}
           id="bubble_home"
-        ></motion.div>
-        <motion.img
+        ></motion.div> */}
+        <motion.div
+          id="img_container"
           initial={{ y: 100 }}
           animate={{
             y: 0,
             transition: { duration: 0.7 },
           }}
           exit={{ opacity: 0, transition: { duration: 0.4 } }}
-          src={main_foto}
-          alt="David Mendoza, main photo"
-          id="main_foto"
-        />
+        >
+          <img src={main_foto} alt="David Mendoza, main photo" id="main_foto" />
+        </motion.div>
         <motion.div
           id="description_home"
           initial={{ opacity: 0.2, y: 500 }}
